d84facf847a7259cde047749c57f3d9652262eda
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-08-02 18:24:41 +0300

    Basic configuration for project

---
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/JOOQConfig.kt
@@ -0,0 +1,58 @@
+package fi.hsl.jore4.timetables.config
+
+import org.jooq.SQLDialect
+import org.jooq.impl.DataSourceConnectionProvider
+import org.jooq.impl.DefaultConfiguration
+import org.jooq.impl.DefaultDSLContext
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
+
+@Configuration
+@ConstructorBinding
+class JOOQConfig(
+    val databaseConfiguration: DatabaseConfiguration,
+    val jooqConfiguration: JOOQConfiguration
+) {
+
+    @Bean
+    fun timetablesDataSource() =
+        DriverManagerDataSource().apply {
+            setDriverClassName(databaseConfiguration.driver)
+            url = databaseConfiguration.url
+            username = databaseConfiguration.username
+            password = databaseConfiguration.password
+        }
+
+    @Bean
+    fun transactionAwareDataSource(): TransactionAwareDataSourceProxy? {
+        return TransactionAwareDataSourceProxy(timetablesDataSource())
+    }
+
+    @Bean
+    fun transactionManager(): DataSourceTransactionManager? {
+        return DataSourceTransactionManager(timetablesDataSource())
+    }
+
+    @Bean
+    fun connectionProvider(): DataSourceConnectionProvider? {
+        return DataSourceConnectionProvider(transactionAwareDataSource())
+    }
+
+    @Bean
+    fun configuration(): DefaultConfiguration? {
+        val config = DefaultConfiguration()
+        config.set(connectionProvider())
+        val dialect = SQLDialect.valueOf(jooqConfiguration.dialect)
+        config.set(dialect)
+        return config
+    }
+
+    @Bean
+    fun dsl(): DefaultDSLContext? {
+        return DefaultDSLContext(configuration())
+    }
+}
---
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/WebSecurityConfig.kt
@@ -0,0 +1,30 @@
+package fi.hsl.jore4.timetables.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            .and()
+            .csrf().disable()
+            .cors().and()
+            .authorizeRequests()
+            .antMatchers(
+                HttpMethod.GET,
+                "/actuator/health",
+                "/hello",
+                "/hello/testi"
+            ).permitAll()
+            .anyRequest().denyAll()
+    }
+}
