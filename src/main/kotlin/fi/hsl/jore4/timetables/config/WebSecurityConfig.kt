c6280cb4d5e8d31262f60a2104d63bdf4b6e5711
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-08-15 18:19:33 +0300

    Upgrade to Spring Boot 3.

--- src/main/kotlin/fi/hsl/jore4/timetables/config/WebSecurityConfig.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/WebSecurityConfig.kt
@@ -1,30 +1,34 @@
-package fi.hsl.jore4.timetables.config
+package fi.hsl.jore4.mapmatching.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
 
-    override fun configure(httpSecurity: HttpSecurity) {
-        httpSecurity
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
-            .and()
-            .csrf().disable()
-            .cors().and()
-            .authorizeRequests()
-            .antMatchers(
-                HttpMethod.GET,
-                "/actuator/health",
-                "/hello",
-                "/hello/test"
-            ).permitAll()
-            .anyRequest().denyAll()
+    @Bean
+    @Throws(Exception::class)
+    fun configure(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.NEVER) }
+            .csrf { it.disable() }
+            .cors {}
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(
+                        HttpMethod.GET,
+                        "/actuator/health",
+                        "/hello",
+                        "/hello/test"
+                    ).permitAll()
+                    .anyRequest().denyAll()
+            }
+            .build()
     }
 }
