f85aaecaac00798b95ffc284b69840526ae1a2c5
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-08-15 18:19:33 +0300

    Upgrade to Spring Boot 3.

--- src/main/kotlin/fi/hsl/jore4/timetables/config/WebConfiguration.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/WebConfiguration.kt
@@ -2,9 +2,11 @@ package fi.hsl.jore4.timetables.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebConfiguration : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
