3b023bbcd8cc4936f87b1d42fd04b047daaf7d6c
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-08-15 18:19:33 +0300

    Upgrade to Spring Boot 3.

--- src/main/kotlin/fi/hsl/jore4/timetables/config/DatabaseConfiguration.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/DatabaseConfiguration.kt
@@ -1,10 +1,8 @@
 package fi.hsl.jore4.timetables.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "jore4.db")
-@ConstructorBinding
 data class DatabaseConfiguration(
     val driver: String,
     val url: String,
--- src/main/kotlin/fi/hsl/jore4/timetables/config/JOOQConfiguration.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/JOOQConfiguration.kt
@@ -1,10 +1,8 @@
 package fi.hsl.jore4.timetables.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "jooq.sql")
-@ConstructorBinding
 data class JOOQConfiguration(
     val dialect: String
 )
