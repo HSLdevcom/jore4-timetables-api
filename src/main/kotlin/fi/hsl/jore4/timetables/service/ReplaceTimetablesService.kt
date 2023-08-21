28f961274dc0841e7ab335956397703f5da8eb06
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-08-21 09:29:46 +0300

    Add initial ReplaceTimetablesService

---
+++ src/main/kotlin/fi/hsl/jore4/timetables/service/ReplaceTimetablesService.kt
@@ -0,0 +1,103 @@
+package fi.hsl.jore4.timetables.service
+
+import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.pojos.VehicleScheduleFrame
+import fi.hsl.jore4.timetables.enumerated.TimetablesPriority
+import fi.hsl.jore4.timetables.repository.VehicleScheduleFrameRepository
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+private val LOGGER = KotlinLogging.logger {}
+
+class InvalidTargetPriorityException(message: String, val targetPriority: TimetablesPriority) : RuntimeException(message)
+
+class StagingVehicleScheduleFrameNotFoundException(message: String, val stagingVehicleScheduleFrameId: UUID) : RuntimeException(message)
+
+@Service
+class ReplaceTimetablesService(
+    private val vehicleScheduleFrameRepository: VehicleScheduleFrameRepository
+) {
+    @Transactional
+    fun replaceTimetables(stagingVehicleScheduleFrameIds: List<UUID>, targetPriority: TimetablesPriority): List<UUID> =
+        stagingVehicleScheduleFrameIds.map {
+            processSingleStagingFrameReplacements(it, targetPriority)
+        }.flatten()
+
+    // This is currently not used from outside this service, only exposed for testing.
+    @Transactional(noRollbackFor = [RuntimeException::class])
+    fun processSingleStagingFrameReplacements(
+        stagingVehicleScheduleFrameId: UUID,
+        targetPriority: TimetablesPriority
+    ): List<UUID> {
+        validateTargetPriority(targetPriority)
+
+        LOGGER.info { "Replacing timetables... $stagingVehicleScheduleFrameId to priority $targetPriority" }
+
+        val stagingFrame = fetchStagingVehicleScheduleFrame(stagingVehicleScheduleFrameId)
+        LOGGER.debug { "Fetched staging frame: $stagingFrame" }
+
+        val replacements = fetchVehicleScheduleFramesToReplace(stagingVehicleScheduleFrameId, targetPriority)
+        LOGGER.info { "Found ${replacements.size} vehicle schedule frames to be replaced." }
+
+        for (replacedFrame in replacements) {
+            LOGGER.info { "Expiring vehicle schedule frame ${replacedFrame.vehicleScheduleFrameId} ..." }
+            expireReplacedFrameBeforeStaging(
+                replacedFrame = replacedFrame,
+                stagingFrame = stagingFrame
+            )
+        }
+
+        LOGGER.info("Promoting staging vehicle schedule frame to target priority...")
+        promoteStagingFrameToPriority(stagingFrame, targetPriority)
+
+        return replacements.map {
+            it.vehicleScheduleFrameId!! // ID of an existing row, can never be null.
+        }
+    }
+
+    @Transactional(readOnly = true)
+    fun fetchVehicleScheduleFramesToReplace(
+        stagingVehicleScheduleFrameId: UUID,
+        targetPriority: TimetablesPriority
+    ): List<VehicleScheduleFrame> {
+        return vehicleScheduleFrameRepository.fetchVehicleScheduleFramesToReplace(
+            stagingVehicleScheduleFrameId,
+            targetPriority
+        )
+    }
+
+    private fun fetchStagingVehicleScheduleFrame(stagingVehicleScheduleFrameId: UUID): VehicleScheduleFrame {
+        val stagingFrame = vehicleScheduleFrameRepository.fetchOneByVehicleScheduleFrameId(stagingVehicleScheduleFrameId)
+        if (stagingFrame != null && stagingFrame.priority == TimetablesPriority.STAGING.value) {
+            return stagingFrame
+        }
+
+        throw StagingVehicleScheduleFrameNotFoundException("Staging vehicle schedule frame not found", stagingVehicleScheduleFrameId)
+    }
+
+    private fun expireReplacedFrameBeforeStaging(
+        replacedFrame: VehicleScheduleFrame,
+        stagingFrame: VehicleScheduleFrame
+    ) {
+        val newEnd = stagingFrame.validityStart.minusDays(1)
+        replacedFrame.validityEnd = newEnd
+        vehicleScheduleFrameRepository.update(replacedFrame)
+    }
+
+    private fun promoteStagingFrameToPriority(
+        stagingFrame: VehicleScheduleFrame,
+        targetPriority: TimetablesPriority
+    ) {
+        stagingFrame.priority = targetPriority.value
+        vehicleScheduleFrameRepository.update(stagingFrame)
+    }
+
+    companion object {
+        private fun validateTargetPriority(targetPriority: TimetablesPriority) {
+            if (targetPriority in listOf(TimetablesPriority.STAGING)) {
+                throw InvalidTargetPriorityException("Can not set priority $targetPriority as target.", targetPriority)
+            }
+        }
+    }
+}
