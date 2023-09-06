35d6430369cd1b67f7a1573f86b73b347e2f318f
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-09-06 14:33:48 +0300

    Add classes for running and using timetables data inserter
    And an example dataset.

---
+++ src/main/kotlin/fi/hsl/jore4/timetables/service/TimetablesDataInserterRunner.kt
@@ -0,0 +1,65 @@
+package fi.hsl.jore4.timetables.service
+
+import fi.hsl.jore4.timetables.config.DatabaseConfiguration
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+import java.io.File
+import java.net.URI
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
+
+private val LOGGER = KotlinLogging.logger {}
+
+@Service
+class TimetablesDataInserterRunner(
+    val databaseConfiguration: DatabaseConfiguration
+) {
+    fun insertDataset(datasetInputFile: String) {
+        LOGGER.info("Inserting dataset from file: $datasetInputFile")
+        val datasetFilePath =
+            Paths.get(datasetInputFile).toAbsolutePath().toString()
+
+        val datasetJson = File(datasetFilePath).readText()
+
+        runTimetablesDataInserter(datasetJson)
+        LOGGER.info("Inserting dataset finished.")
+    }
+
+    fun runTimetablesDataInserter(datasetJson: String): String {
+        val inserterWorkDirectory = Paths.get("jore4-hasura/test/hasura")
+            .toAbsolutePath().toString()
+        val command = "yarn --silent timetables-data-inserter:cli insert hsl ${buildDatabaseArguments()}"
+
+        var process = ProcessBuilder(*command.split(" ").toTypedArray())
+            .directory(File(inserterWorkDirectory))
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+
+        process.outputStream.bufferedWriter().use { writer ->
+            writer.write(datasetJson)
+            writer.flush()
+        }
+
+        process.waitFor(10, TimeUnit.SECONDS)
+
+        val output = process.inputStream.bufferedReader().use { reader ->
+            reader.readText()
+        }
+
+        return output
+    }
+
+    private fun buildDatabaseArguments(): String {
+        val cleanJdbcUri = databaseConfiguration.url.substring(5) // Strip leading "jdbc:"
+        val jdbcUri = URI.create(cleanJdbcUri)
+        val databaseName = jdbcUri.path.substring(1) // Strip leading "/"
+
+        return listOf(
+            "--host ${jdbcUri.host}",
+            "--port ${jdbcUri.port}",
+            "--database $databaseName",
+            "--user ${databaseConfiguration.username}",
+            "--password ${databaseConfiguration.password}"
+        ).joinToString(" ")
+    }
+}
