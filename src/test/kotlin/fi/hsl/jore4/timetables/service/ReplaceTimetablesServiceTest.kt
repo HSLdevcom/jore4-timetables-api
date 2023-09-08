80d08db183a0f9f605e97cea20405ffd90b54595
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-09-08 10:26:09 +0300

    TODO integration test activity profile

--- src/test/kotlin/fi/hsl/jore4/timetables/service/ReplaceTimetablesServiceTest.kt
+++ src/test/kotlin/fi/hsl/jore4/timetables/service/ReplaceTimetablesServiceTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDate
 import java.util.*
 import kotlin.test.assertContains
@@ -23,6 +24,7 @@ import kotlin.test.assertNotNull
 private val LOGGER = KotlinLogging.logger {}
 
 @SpringBootTest
+@ActiveProfiles("integration-test")
 class ReplaceTimetablesServiceTest @Autowired constructor(
     val dsl: DSLContext,
     val replaceTimetablesService: ReplaceTimetablesService,
