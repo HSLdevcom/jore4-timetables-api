4f54070e2bf751d2951ae8d815debfc1565ac5ef
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-09-15 11:34:44 +0300

    Use consistent naming for `@Configuration`-annotated classes.

--- src/main/kotlin/fi/hsl/jore4/timetables/config/WebConfig.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/WebConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class WebConfiguration : WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**").allowedMethods("*")
