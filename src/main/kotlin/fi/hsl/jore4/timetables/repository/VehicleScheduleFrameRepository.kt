22391e36cb26c4e15e26d5e9b2f275354d245817
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-09-28 13:41:01 +0300

    Import table from companion object to reduce some jOOQ bloat

--- src/main/kotlin/fi/hsl/jore4/timetables/repository/VehicleScheduleFrameRepository.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/repository/VehicleScheduleFrameRepository.kt
@@ -2,6 +2,7 @@ package fi.hsl.jore4.timetables.repository
 
 import fi.hsl.jore.jore4.jooq.vehicle_schedule.routines.references.getOverlappingSchedules
 import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.GetOverlappingSchedules
+import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.VehicleScheduleFrame.Companion.VEHICLE_SCHEDULE_FRAME
 import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.daos.VehicleScheduleFrameDao
 import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.pojos.VehicleScheduleFrame
 import fi.hsl.jore4.timetables.enumerated.TimetablesPriority
@@ -13,10 +14,10 @@ import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
-import fi.hsl.jore.jore4.jooq.vehicle_schedule.tables.VehicleScheduleFrame as VehicleScheduleFrameTable
 
 @Repository
-class VehicleScheduleFrameRepository(private val dsl: DSLContext, config: DefaultConfiguration) : VehicleScheduleFrameDao(config) {
+class VehicleScheduleFrameRepository(private val dsl: DSLContext, config: DefaultConfiguration) :
+    VehicleScheduleFrameDao(config) {
     @Transactional(readOnly = true, propagation = Propagation.MANDATORY)
     fun fetchVehicleScheduleFramesToReplace(
         stagingVehicleScheduleFrameId: UUID,
@@ -31,8 +32,8 @@ class VehicleScheduleFrameRepository(private val dsl: DSLContext, config: Defaul
         val replacedVehicleScheduleFrameIdField =
             DSL.field(replacedFrameIdName, UUID::class.java)
 
-        val stagingFrame = VehicleScheduleFrameTable.VEHICLE_SCHEDULE_FRAME.`as`("staging")
-        val replacedFrame = VehicleScheduleFrameTable.VEHICLE_SCHEDULE_FRAME.`as`("replaced")
+        val stagingFrame = VEHICLE_SCHEDULE_FRAME.`as`("staging")
+        val replacedFrame = VEHICLE_SCHEDULE_FRAME.`as`("replaced")
 
         val framesReplacedByStagingQuery = dsl
             .with(
@@ -66,44 +67,24 @@ class VehicleScheduleFrameRepository(private val dsl: DSLContext, config: Defaul
             )
             .from(replacedFrame)
             .join(overlappingSchedulesCTE)
-            .on(
-                replacedFrame
-                    .VEHICLE_SCHEDULE_FRAME_ID
-                    .eq(replacedVehicleScheduleFrameIdField)
-            )
+            .on(replacedFrame.VEHICLE_SCHEDULE_FRAME_ID.eq(replacedVehicleScheduleFrameIdField))
             .join(stagingFrame)
-            .on(
-                stagingFrame
-                    .VEHICLE_SCHEDULE_FRAME_ID
-                    .eq(stagingVehicleScheduleFrameIdField)
-            )
-            .where(
-                replacedFrame.PRIORITY
-                    .eq(targetPriority.value)
-            )
+            .on(stagingFrame.VEHICLE_SCHEDULE_FRAME_ID.eq(stagingVehicleScheduleFrameIdField))
+            .where(replacedFrame.PRIORITY.eq(targetPriority.value))
             // Must be able to update the validity time of replaced frame,
             // so it ends before staging starts.
             // Thus, also needs to start before,
             // so a valid validity range can be set without touching start time.
-            .and(
-                replacedFrame.VALIDITY_START
-                    .lessThan(stagingFrame.VALIDITY_START)
-            )
+            .and(replacedFrame.VALIDITY_START.lessThan(stagingFrame.VALIDITY_START))
 
         return dsl
             .select()
             // Returns a row for each day type id.
             // We are not interested in those here, just the replaced frame ids.
             .distinctOn(replacedVehicleScheduleFrameIdField)
-            .from(
-                framesReplacedByStagingQuery
-            )
-            .join(VehicleScheduleFrameTable.VEHICLE_SCHEDULE_FRAME)
-            .on(
-                VehicleScheduleFrameTable.VEHICLE_SCHEDULE_FRAME
-                    .VEHICLE_SCHEDULE_FRAME_ID
-                    .eq(replacedVehicleScheduleFrameIdField)
-            )
+            .from(framesReplacedByStagingQuery)
+            .join(VEHICLE_SCHEDULE_FRAME)
+            .on(VEHICLE_SCHEDULE_FRAME.VEHICLE_SCHEDULE_FRAME_ID.eq(replacedVehicleScheduleFrameIdField))
             .fetchInto(VehicleScheduleFrame::class.java)
     }
 }
