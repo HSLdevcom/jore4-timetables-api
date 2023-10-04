6ee56403d3a50f37c07bcc4a66356bb83c70e033
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-10-04 13:18:44 +0300

    Rename dataset insert method
    To more clearly show that it actually truncates the whole DB as well.

--- src/main/kotlin/fi/hsl/jore4/timetables/service/TimetablesDataInserterRunner.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/service/TimetablesDataInserterRunner.kt
@@ -14,9 +14,10 @@ private val LOGGER = KotlinLogging.logger {}
 class TimetablesDataInserterRunner(
     val databaseConfiguration: DatabaseProperties
 ) {
-    fun runTimetablesDataInserter(datasetJson: String): String {
+    fun truncateAndInsertDataset(datasetJson: String): String {
         val inserterWorkDirectory = Paths.get("jore4-hasura/test/hasura")
             .toAbsolutePath().toString()
+        // Note: this performs DB truncation internally.
         val command = "yarn --silent timetables-data-inserter:cli insert hsl ${buildDatabaseArguments()}"
 
         val process = ProcessBuilder(*command.split(" ").toTypedArray())
