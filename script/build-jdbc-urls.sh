b8832046dc6191893c9273698d02b083872dccec
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-10-16 22:15:23 +0300

    Replace the database user and its password in application configuration.
    The change is done in order to spot possible database permission issues.
    Also, remove defaulting to Hasura user in Docker setup.
    
    Resolves HSLdevcom/jore4#1551

--- script/build-jdbc-urls.sh
+++ script/build-jdbc-urls.sh
@@ -6,12 +6,6 @@ set -eu
 
 # if JORE4_DB_URL was not set, build it from parts
 if [ -z "${JORE4_DB_URL+x}" ]; then
-  export JORE4_DB_HOSTNAME=${JORE4_DB_HOSTNAME:-"jore4-testdb"}
   export JORE4_DB_PORT=${JORE4_DB_PORT:-"5432"}
-  export JORE4_DB_DATABASE=${JORE4_DB_DATABASE:-"timetablesdb"}
   export JORE4_DB_URL="jdbc:postgresql://${JORE4_DB_HOSTNAME}:${JORE4_DB_PORT}/${JORE4_DB_DATABASE}?stringtype=unspecified"
 fi
-
-# Set dev auth if it is not defined
-export JORE4_DB_USERNAME=${JORE4_DB_USERNAME:-"dbhasura"}
-export JORE4_DB_PASSWORD=${JORE4_DB_PASSWORD:-"hasurapassword"}
