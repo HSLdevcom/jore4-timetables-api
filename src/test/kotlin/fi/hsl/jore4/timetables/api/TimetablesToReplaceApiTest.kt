27678c62f3ce8c64690945a8e7f8096b14e58f79
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-10-18 19:41:51 +0300

    Add test specific applicaiton properties
    Add test-application.properties and the profile definition for tests
    to use it.

--- src/test/kotlin/fi/hsl/jore4/timetables/api/TimetablesToReplaceApiTest.kt
+++ src/test/kotlin/fi/hsl/jore4/timetables/api/TimetablesToReplaceApiTest.kt
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -24,6 +25,7 @@ import java.util.UUID
 @ExtendWith(MockKExtension::class)
 @AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("test")
 class TimetablesToReplaceApiTest(@Autowired val mockMvc: MockMvc) {
     @MockkBean
     private lateinit var replaceTimetablesService: ReplaceTimetablesService
