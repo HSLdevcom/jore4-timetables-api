8cb5b7834e2c011b6b9c4a8ae2db2bc1713c4166
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-10-19 12:47:51 +0300

    Fix a prefix in `application-test.properties`.

--- src/test/kotlin/fi/hsl/jore4/timetables/config/DataInserterDatabaseProperties.kt
+++ src/test/kotlin/fi/hsl/jore4/timetables/config/DataInserterDatabaseProperties.kt
@@ -2,7 +2,7 @@ package fi.hsl.jore4.timetables.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "dataInserter.db")
+@ConfigurationProperties(prefix = "data-inserter.db")
 data class DataInserterDatabaseProperties(
     val username: String,
     val password: String
