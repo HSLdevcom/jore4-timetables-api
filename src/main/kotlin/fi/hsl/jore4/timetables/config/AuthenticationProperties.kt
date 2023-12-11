0f461dc3821ad652ea5ff80c795382bf4f05f81e
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-12-11 15:08:21 +0200

    Add support for changing auth url

---
+++ src/main/kotlin/fi/hsl/jore4/timetables/config/AuthenticationProperties.kt
@@ -0,0 +1,8 @@
+package fi.hsl.jore4.timetables.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "auth")
+data class AuthenticationProperties(
+    val url: String
+)
