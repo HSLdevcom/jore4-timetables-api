45061fdf498d938e7b62377f18f5154d3b605567
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-12-14 07:47:41 +0200

    Separate type from HasuraErrorExtensions and Response
    To be more explicit that it's not related to Hasura itself,
    just our own extension property.

--- src/main/kotlin/fi/hsl/jore4/timetables/api/util/HasuraErrorResponse.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/api/util/HasuraErrorResponse.kt
@@ -1,9 +1,14 @@
 package fi.hsl.jore4.timetables.api.util
 
 // See https://hasura.io/docs/latest/actions/action-handlers/#returning-an-error-response
-class HasuraErrorResponse(
+open class HasuraErrorResponse(
     nullableMessage: String?,
-    val extensions: HasuraErrorExtensions
+    open val extensions: HasuraErrorExtensions
 ) {
     val message = nullableMessage ?: "An error occurred."
 }
+
+class JoreErrorResponse(
+    nullableMessage: String?,
+    override val extensions: JoreErrorExtensions
+) : HasuraErrorResponse(nullableMessage, extensions)
