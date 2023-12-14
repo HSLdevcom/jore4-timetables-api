e54cf60f54e1b48a65dfc0dedc2a252e3101e69c
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-12-14 07:50:29 +0200

    Rename the error type enum
    Since this doesn't have anything to do with Hasura itself,
    other than that it is included in responses that go through Hasura.

--- src/main/kotlin/fi/hsl/jore4/timetables/api/util/HasuraErrorExtensions.kt
+++ src/main/kotlin/fi/hsl/jore4/timetables/api/util/HasuraErrorExtensions.kt
@@ -18,30 +18,30 @@ sealed interface HasuraErrorExtensions {
 sealed interface JoreErrorExtensions : HasuraErrorExtensions {
     // Not required by Hasura,  but we always want to provide one
     // so that the UI can present nice detailed error messages.
-    val type: HasuraErrorType
+    val type: TimetablesApiErrorType
 }
 
 data class PlainStatusExtensions(
     override val code: Int,
-    override val type: HasuraErrorType
+    override val type: TimetablesApiErrorType
 ) : JoreErrorExtensions {
 
     constructor(httpStatus: HttpStatus) : this(
         httpStatus.value(),
-        HasuraErrorType.UnknownError
+        TimetablesApiErrorType.UnknownError
     )
 }
 
 data class InvalidTargetPriorityExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val targetPriority: TimetablesPriority
 ) : JoreErrorExtensions {
 
     companion object {
         fun from(ex: InvalidTargetPriorityException) = InvalidTargetPriorityExtensions(
             HttpStatus.BAD_REQUEST.value(),
-            HasuraErrorType.InvalidTargetPriorityError,
+            TimetablesApiErrorType.InvalidTargetPriorityError,
             ex.targetPriority
         )
     }
@@ -49,14 +49,14 @@ data class InvalidTargetPriorityExtensions(
 
 data class StagingVehicleScheduleFrameNotFoundExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val stagingVehicleScheduleFrameId: UUID
 ) : JoreErrorExtensions {
 
     companion object {
         fun from(ex: StagingVehicleScheduleFrameNotFoundException) = StagingVehicleScheduleFrameNotFoundExtensions(
             HttpStatus.NOT_FOUND.value(),
-            HasuraErrorType.StagingVehicleScheduleFrameNotFoundError,
+            TimetablesApiErrorType.StagingVehicleScheduleFrameNotFoundError,
             ex.stagingVehicleScheduleFrameId
         )
     }
@@ -64,14 +64,14 @@ data class StagingVehicleScheduleFrameNotFoundExtensions(
 
 data class TargetVehicleScheduleFrameNotFoundExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val stagingVehicleScheduleFrameId: UUID
 ) : JoreErrorExtensions {
 
     companion object {
         fun from(ex: TargetFrameNotFoundException) = TargetVehicleScheduleFrameNotFoundExtensions(
             HttpStatus.NOT_FOUND.value(),
-            HasuraErrorType.TargetVehicleScheduleFrameNotFoundError,
+            TimetablesApiErrorType.TargetVehicleScheduleFrameNotFoundError,
             ex.stagingVehicleScheduleFrameId
         )
     }
@@ -79,7 +79,7 @@ data class TargetVehicleScheduleFrameNotFoundExtensions(
 
 data class MultipleTargetFramesFoundExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val stagingVehicleScheduleFrameId: UUID,
     val targetVehicleScheduleFrameIds: List<UUID>
 ) : JoreErrorExtensions {
@@ -87,7 +87,7 @@ data class MultipleTargetFramesFoundExtensions(
     companion object {
         fun from(ex: MultipleTargetFramesFoundException) = MultipleTargetFramesFoundExtensions(
             HttpStatus.CONFLICT.value(),
-            HasuraErrorType.MultipleTargetFramesFoundError,
+            TimetablesApiErrorType.MultipleTargetFramesFoundError,
             ex.stagingVehicleScheduleFrameId,
             ex.targetVehicleScheduleFrameIds
         )
@@ -96,14 +96,14 @@ data class MultipleTargetFramesFoundExtensions(
 
 data class TargetPriorityParsingExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val targetPriority: Int
 ) : JoreErrorExtensions {
 
     companion object {
         fun from(ex: TargetPriorityParsingException) = TargetPriorityParsingExtensions(
             HttpStatus.BAD_REQUEST.value(),
-            HasuraErrorType.TargetPriorityParsingError,
+            TimetablesApiErrorType.TargetPriorityParsingError,
             ex.targetPriority
         )
     }
@@ -111,34 +111,34 @@ data class TargetPriorityParsingExtensions(
 
 data class TransactionSystemExtensions(
     override val code: Int,
-    override val type: HasuraErrorType,
+    override val type: TimetablesApiErrorType,
     val sqlErrorMessage: String
 ) : JoreErrorExtensions {
 
     companion object {
         fun from(ex: TransactionSystemException) = TransactionSystemExtensions(
             HttpStatus.CONFLICT.value(),
-            resolveHasuraErrorType(ex),
+            resolveErrorType(ex),
             ex.cause?.message ?: "unknown error on transaction commit or rollback"
         )
 
-        private fun resolveHasuraErrorType(ex: TransactionSystemException): HasuraErrorType {
+        private fun resolveErrorType(ex: TransactionSystemException): TimetablesApiErrorType {
             val errorMessage = ex.cause?.message ?: "" // Should always be defined though.
 
             // Attempt to detect from the error message (because there's no other data really) which error case triggered the failure.
             // Mainly using either the triggering validation function name or some known part of the error message for this.
             return errorTypesWithMatchingSubstrings.find { it.second in errorMessage }?.first
-                ?: HasuraErrorType.TransactionSystemError
+                ?: TimetablesApiErrorType.TransactionSystemError
         }
 
         private val errorTypesWithMatchingSubstrings = listOf(
-            HasuraErrorType.PassingTimeStopPointMatchingOrderError to "passing times and their matching stop points must be in same order",
-            HasuraErrorType.PassingTimeFirstArrivalTimeError to "first passing time must not have arrival_time set",
-            HasuraErrorType.PassingTimeLastDepartureTimeError to "last passing time must not have departure_time set",
-            HasuraErrorType.PassingTimeNullError to "all passing time that are not first or last in the sequence must have both departure and arrival time defined",
-            HasuraErrorType.PassingTimesMixedJourneyPatternRefsError to "inconsistent journey_pattern_ref within vehicle journey, all timetabled_passing_times must reference same journey_pattern_ref as the vehicle_journey",
-            HasuraErrorType.ConflictingSchedulesError to "validate_queued_schedules_uniqueness",
-            HasuraErrorType.SequentialIntegrityError to "validate_service_sequential_integrity"
+            TimetablesApiErrorType.PassingTimeStopPointMatchingOrderError to "passing times and their matching stop points must be in same order",
+            TimetablesApiErrorType.PassingTimeFirstArrivalTimeError to "first passing time must not have arrival_time set",
+            TimetablesApiErrorType.PassingTimeLastDepartureTimeError to "last passing time must not have departure_time set",
+            TimetablesApiErrorType.PassingTimeNullError to "all passing time that are not first or last in the sequence must have both departure and arrival time defined",
+            TimetablesApiErrorType.PassingTimesMixedJourneyPatternRefsError to "inconsistent journey_pattern_ref within vehicle journey, all timetabled_passing_times must reference same journey_pattern_ref as the vehicle_journey",
+            TimetablesApiErrorType.ConflictingSchedulesError to "validate_queued_schedules_uniqueness",
+            TimetablesApiErrorType.SequentialIntegrityError to "validate_service_sequential_integrity"
         )
     }
 }
